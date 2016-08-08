3eefd4bd0e26b6ae0c3f5d5f3aea4e766a793a34
yotamfe <yotam.feldman@gmail.com>
2016-08-08 14:54:53 +0300

    Start. :)

--- ivy/ivy_check.py
+++ ivy/ivy_check.py
@@ -52,7 +52,7 @@ def check_conjectures(kind,msg,ag,state):
             agui.try_conjecture(state,msg="{}\nChoose one to see counterexample.".format(msg))
             gui.tk.mainloop()
             exit(1)
-        raise iu.IvyError(None,"{} failed.".format(kind))
+        raise iu.IvyError(None,"{} failed, {}".format(kind,msg))
     
 def usage():
     print "usage: \n  {} file.ivy".format(sys.argv[0])
@@ -96,7 +96,20 @@ def check_module():
                     if cex is not None:
                         display_cex("safety failed",cex)
                     check_conjectures('Consecution','These conjectures are not inductive.',ag,ag.states[-1])
-
+                    
+def infer_safe_summaries():
+    ivy_isolate.create_isolate(None)
+    check_properties()
+    ag = ivy_art.AnalysisGraph(initializer=ivy_alpha.alpha)
+    with ivy_interp.EvalContext(check=False):
+        #check_conjectures('Initiation','These conjectures are false initially.',ag,ag.states[0])
+        for a in sorted(im.module.public_actions):
+            print "trying {}...".format(a)
+            ag.execute_action(a,prestate=ag.states[0])
+            cex = ag.check_bounded_safety(ag.states[-1])
+            if cex is not None:
+                display_cex("safety failed",cex)
+            check_conjectures('Consecution','These conjectures are not inductive.',ag,ag.states[-1])
 
 def main():
     ivy.read_params()
@@ -106,7 +119,8 @@ def main():
     with im.Module():
         with utl.ErrorPrinter():
             ivy.source_file(sys.argv[1],ivy.open_read(sys.argv[1]),create_isolate=False)
-            check_module()
+            #check_module()
+            infer_safe_summaries()
     print "OK"
 
 
