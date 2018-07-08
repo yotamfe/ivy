59e15e2f98e736e0a9138af664125891995fda31
yotamfe <yotam.feldman@gmail.com>
2018-07-08 20:51:21 +0300

    Should not be necessary to close universal quantifiers there, but we'll see...

--- ivy/ivy_linear_pdr.py
+++ ivy/ivy_linear_pdr.py
@@ -484,7 +484,7 @@ class LinearPdr(ivy_infer.PdrElements):
             assert not (set(disjunct_map_edge.keys()) & set(disjunct_map.keys()))
             disjunct_map.update(disjunct_map_edge)
 
-            all_transformability_combined.append(ivy_transrel.conjoin(prestate_summaries[lhs_pred].get_summary().to_single_clauses(close=True).closed_universals(),
+            all_transformability_combined.append(ivy_transrel.conjoin(prestate_summaries[lhs_pred].get_summary().to_single_clauses().closed_universals(),
                                                                       all_transformability_along_edge))
         # all_transformability_combined = ivy_logic_utils.or_clauses(*transformability_clauses_unified)
         all_transformability_clauses, _ = ivy_logic_utils.tagged_or_clauses_with_mapping('all-edge', *all_transformability_combined)
